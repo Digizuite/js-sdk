899b8d5eb2ed651a75feb5aa6fc70f986dbfa9db
Stefan Fodor <sf@house-co.dk>
2017-06-02 13:08:35 +0200

    hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,73 @@
+/* global __dirname, require, module  */
 const path    = require('path');
+const BabiliPlugin = require('babili-webpack-plugin');
 
-module.exports = {
-	entry : "index.js",
-	output: {
-		path          : path.resolve(__dirname, 'dist'),
-		filename      : 'digizuite.js',
-		library       : 'Digizuite',
-		libraryTarget : 'umd',
-		umdNamedDefine: true
-	},
+const ENV_PRODUCTION = 'prod';
+const ENV_DEVELOPMENT = 'dev';
+
+/**
+ * Get webpack configs
+ * @param env
+ * @returns {*}
+ */
+function getWebpackConfig(env) {
 	
-	resolve: {
-		modules   : [
-			'src/', 'node_modules'
-		],
-		extensions: ['.js']
-	},
+	const plugins = [];
 	
-	resolveLoader: {
-		modules: [
-			'node_modules',
-			path.resolve(__dirname, "src")
-		]
-	},
+	if( env === ENV_PRODUCTION ) {
+		plugins.push( new BabiliPlugin({
+			mangle       : true,
+			keepFnName   : true,
+			keepClassName: true
+		}) );
+	}
 	
-	module: {
-		rules: [],
-	},
+	return 	{
+		entry : 'index.js',
+		output: {
+			path          : path.resolve(__dirname, 'dist'),
+			filename      : `digizuite${ env === ENV_PRODUCTION ? '.min' : ''}.js`,
+			library       : 'Digizuite',
+			libraryTarget : 'umd',
+			umdNamedDefine: true
+		},
+		
+		resolve: {
+			modules   : [
+				'src/', 'node_modules'
+			],
+			extensions: ['.js']
+		},
+		
+		resolveLoader: {
+			modules: [
+				'node_modules',
+				path.resolve(__dirname, 'src')
+			]
+		},
+		
+		module: {
+			rules: [],
+		},
+		
+		plugins,
+		
+		devtool: 'cheap-source-map',
+	};
+}
+
+/**
+ * Exports
+ * @param env
+ * @returns {[*]}
+ */
+module.exports = function(env) {
 	
-	plugins: [],
+	const configs = [getWebpackConfig(ENV_DEVELOPMENT)];
 	
-	devtool: 'cheap-source-map',
+	if( env === ENV_PRODUCTION ) {
+		configs.push(getWebpackConfig(ENV_PRODUCTION));
+	}
 	
+	return configs;
 };
