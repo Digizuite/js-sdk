ed9426f073171fb5ce1a54449b03b6beb475c192
Stefan Fodor <sf@house-co.dk>
2017-06-20 14:59:27 +0200

    Fix incorrect metadata type in money

--- src/model/metadata/moneyMetadataItem.js
+++ src/model/metadata/moneyMetadataItem.js
@@ -5,8 +5,8 @@ export class MoneyMetadataItem extends StringMetadataItem {
 	static get TYPE() { return 62; }
 	static get VALUE_TYPE() { return 1; }
 	
-	get TYPE() { return StringMetadataItem.TYPE; }
-	get VALUE_TYPE() { return StringMetadataItem.VALUE_TYPE; }
+	get TYPE() { return MoneyMetadataItem.TYPE; }
+	get VALUE_TYPE() { return MoneyMetadataItem.VALUE_TYPE; }
 	
 	constructor( args = {} ) {
 		super(args);
