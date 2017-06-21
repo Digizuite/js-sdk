5dba02e7f1afc8bc18e41a3a8dc9064f9d77d532
Stefan Fodor <sf@house-co.dk>
2017-06-21 14:46:05 +0200

    MM-2881 Ability to use multiple containers

--- src/endpoint/batch.js
+++ src/endpoint/batch.js
@@ -17,21 +17,32 @@ export class Batch extends Endpoint {
 	/**
 	 * Make a batch update
 	 * @param {Object} args
-	 * @param {UpdateContainer} args.batch
+	 * @param {UpdateContainer[]} args.containers
 	 * @return {Promise}
 	 */
 	update( args ) {
 		
-		if( !(args.batch instanceof UpdateContainer) ) {
-			throw new Error('update expects a batch parameter');
+		// Ensure that we have containers in the format we want
+		if(
+			!args.hasOwnProperty('containers') ||
+			!Array.isArray(args.containers) ||
+			args.containers.length === 0
+		) {
+			throw new Error('Batch expects an array of containers as parameter.');
 		}
+		
+		args.containers.forEach((thisContainer) => {
+			if( !(thisContainer instanceof UpdateContainer)) {
+				throw new Error('Batch expects containers to be of type UpdateContainer.');
+			}
+		});
 		
 		const batchUpdateRequest = new BatchUpdate({
 			apiUrl : this.apiUrl,
 		});
 		
 		return batchUpdateRequest.execute({
-			batch : args.batch
+			containers : args.containers
 		});
 		
 	}
