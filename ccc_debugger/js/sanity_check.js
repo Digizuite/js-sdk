ef9f12d3e5de83a1668d282f0efb5f0985c90140
Stefan Fodor <stefan@unserialized.dk>
2019-07-12 10:33:12 +0300

    Added fetching of folders and assets in sanity check

---
+++ ccc_debugger/js/sanity_check.js
@@ -0,0 +1,46 @@
+(async function(globals){
+
+    const instance = await globals.connectorInstance;
+
+    await checkFolders(instance);
+    await checkAssets(instance);
+
+    /**
+     * Check folders
+     * @param instance
+     * @returns {Promise<void>}
+     */
+    async function checkFolders(instance) {
+        try {
+            const foldersResp = await instance.content.getFolders({
+                path: '/'
+            });
+
+            console.log(`Fetched ${foldersResp.folders.length} folders.`);
+            console.debug(foldersResp);
+        } catch (e) {
+            console.error(`Error loading folders. ${e}`)
+        }
+    }
+
+    /**
+     * Check folders
+     * @param instance
+     * @returns {Promise<void>}
+     */
+    async function checkAssets(instance) {
+        try {
+            const assetsResp = await instance.content.getAssets({
+                navigation: {
+                    page: 1,
+                    limit: 12
+                },
+            });
+
+            console.log(`Fetched ${assetsResp.assets.length} of ${assetsResp.navigation.total} assets successfully.`);
+        } catch (e) {
+            console.error(`Error loading assets. ${e}`)
+        }
+    }
+
+}(window));
