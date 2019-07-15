dc381149572a99b60c61e6c8f7e25c5106dbcfff
admin <admin@TeamCity>
2019-07-15 09:54:50 +0200

    TeamCity change in 'JS SDK' project: runners of 'Create CCC Debugger' build configuration were updated

--- .teamcity/patches/buildTypes/CreateCccDebugger.kts
+++ .teamcity/patches/buildTypes/CreateCccDebugger.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.powerShell
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -28,6 +29,12 @@ create(DslContext.projectId, BuildType({
             name = "NPM Build"
             type = "jonnyzzz.npm"
             param("npm_commands", "ci")
+        }
+        powerShell {
+            name = "Create Release package"
+            scriptMode = file {
+                path = "build_scripts/CreateReleasePackage.ps1"
+            }
         }
     }
 }))
