bb8439ffa2ee1ab552a2d6361d897c085d731d3b
admin <admin@TeamCity>
2019-12-06 15:58:03 +0100

    TeamCity change in 'JS SDK' project: general settings of 'Create CCC Debugger' build configuration were updated

--- .teamcity/patches/buildTypes/CreateCccDebugger.kts
+++ .teamcity/patches/buildTypes/CreateCccDebugger.kts
@@ -13,7 +13,7 @@ create(DslContext.projectId, BuildType({
     id("CreateCccDebugger")
     name = "Create CCC Debugger"
 
-    artifactRules = "Publish/Zip/**.zip"
+    artifactRules = "ccc_debugger/**/* => ccc_debugger.zip"
 
     vcs {
         root(DslContext.settingsRoot)
