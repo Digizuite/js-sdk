5673f418f8c90f1b41e719ff137fa285e793f527
zlepper <hansen13579@gmail.com>
2020-02-18 15:06:06 +0100

    Only build actual branches

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -78,6 +78,7 @@ object Build : BuildType({
                 -:package.json
                 -:comment=Update to \d+.\d+.\d+:**
             """.trimIndent()
+            branchFilter = "+:refs/heads/*"
         }
     }
 })
